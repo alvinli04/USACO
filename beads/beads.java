caff9a2b200bea1c81a4a6ea8befcd6862c4ce71
alvinli04 <alvin.li.j@gmail.com>
2019-11-27 22:21:55 -0500

    beads update

--- beads/beads.java
+++ beads/beads.java
@@ -8,31 +8,51 @@ import java.io.*;
 
 public class beads{
         public static void main(String[] args) throws Exception{
+                //IO
                 BufferedReader br = new BufferedReader(new FileReader("beads.in"));
                 FileWriter fw = new FileWriter(new File("beads.out"));
                 int beadNum = Integer.parseInt(br.readLine());
                 String input = br.readLine();
-                input.concat(input);
-                char[] necklace = input.toCharArray();
-                int highestSum = 0;
-                for(int i = 0; i < necklace.length - 1; i++){
-                        int backsum = 0;
-                        int fdsum = 0;
-                        // Iterate backwards
-                        if(necklace[i] == 'b' && necklace[i+1] == 'r' || necklace[i] == 'r' && necklace[i+1] == 'b'){
-                                for(int j = i; (necklace[j] == necklace[i] || necklace[j] == 'w') && j > 0; j--){
-                                        backsum++;
+
+                input = input + input; //doubles string so it loops like a circle
+                //System.out.println(input);
+                int plateau1 = 0; // stores p2
+                int plateau2 = 0; //updates continuously
+                int max = 0; //stores max 2 plateau val
+                int initPos = 0; //initial pos block thing
+
+                for(int i = 1; i < input.length(); i++){
+                        //forms plateaus after a string of whites and other color
+                        if(input.charAt(initPos) == input.charAt(i) || input.charAt(i) == 'w'){
+                                continue;
+                        }
+                        else{
+                                //go back for whites by looping back a little
+                                int numW = 0;
+                                for(int j = i - 1; input.charAt(j) == 'w' && j >= 0; j--){
+                                        numW++;
                                 }
-                                for(int j = i + 1; (necklace[j] == necklace[i+1] || necklace[j] == 'w') && j < necklace.length - 1; j++){
-                                        fdsum++;
-                                }
-                                highestSum = Math.max(highestSum, fdsum + backsum);
+                                //System.out.println(numW);
+
+                                //stores last plateau and starts making a new one
+                                plateau1 = plateau2;
+                                plateau2 = i - initPos;
+                                initPos = i;
+
+                                //update max
+                                max = Math.max(max, plateau1 + plateau2 + numW);
                         }
                 }
-                fw.write(highestSum + 1 + "\n");
+
+                if(plateau1 == 0){
+                        fw.write(input.length() / 2 + "\n");
+                        //if homogeneous it's the whole thing
+                }
+                else{
+                        fw.write(max + "\n");
+                }
                 br.close();
                 fw.close();
-                }
-
-        //rrrbr
+                //wwwbbrwrbrbrrbrbrwrwwrbwrwrrbwwwbbrwrbrbrrbrbrwrwwrbwrwrrb
+        }
 }
