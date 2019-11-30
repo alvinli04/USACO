907c46366f035ec2afa2bf135e02b7c7318de244
alvinli <alvin.li.j@gmail.com>
2019-11-29 21:54:49 -0500

    finished transform

--- transform/transform.java
+++ transform/transform.java
@@ -21,25 +21,26 @@ public class transform{
                 for(int i = 0; i < n; i++){
                         end[i] = br.readLine().toCharArray();
                 }
+                //start = reflect(start);
                 //make the arrays
-
+                char[][] ref = reflect(start);
                 //checks
-                if(Arrays.deepEquals(end, rot(start))){
+                if(Arrays.deepEquals(end, rot90(start))){
                         fw.write(1 + "\n");
                 }
-                else if(Arrays.deepEquals(end, rot(rot(start)))){
+                else if(Arrays.deepEquals(end, rot180(start))){
                         fw.write(2 + "\n");
                 }
-                else if(Arrays.deepEquals(end, rot(rot(start)))){
+                else if(Arrays.deepEquals(end, rot270(start))){
                         fw.write(3 + "\n");
                 }
                 else if(Arrays.deepEquals(end, reflect(start))){
                         fw.write(4 + "\n");
                 }
                 else if(
-                (Arrays.deepEquals(end, reflect(rot(start))))||
-                (Arrays.deepEquals(end, reflect(rot(rot(start)))))||
-                (Arrays.deepEquals(end, reflect(rot(rot(rot(start))))))
+                (Arrays.deepEquals(end, rot90(ref)))||
+                (Arrays.deepEquals(end, rot180(ref)))||
+                (Arrays.deepEquals(end, rot270(ref)))
                 ){
                         fw.write(5 + "\n");
                 }
@@ -52,29 +53,24 @@ public class transform{
                 fw.close();
                 br.close();
 
-                System.out.println("end");
-                for(char[] i: end){
-                        for(char j: i){
-                                System.out.print(j);
-                        }
-                        System.out.println();
-                }
-                System.out.println(Arrays.deepEquals(end, rot(start)));
+                //brint(start);
+                brint(reflect(rot270(start)));
+                System.out.println();
+                brint(end);
+                System.out.println("deepEquals: " + Arrays.deepEquals(end, reflect(rot270(start))));
         }
 
         public static char[][] reflect(char[][] input){
-                char[][] arr = input;
-                for(char[] i: arr){
-                        for(int j = 0; j < i.length / 2; j++){
-                                char n = i[j];
-                                i[j] = i[i.length - j - 1];
-                                i[i.length - j - 1] = n;
+                char[][] arr = new char[input.length][input.length];
+                for(int i = 0; i < input.length; i++){
+                        for(int j = 0; j < input.length; j++){
+                                arr[i][j] = input[i][input.length - j - 1];
                         }
                 }
                 return arr;
         }
 
-        public static char[][] rot(char[][] input){
+        public static char[][] rot90(char[][] input){
                 char[][] arr = new char[input.length][input[0].length];
                 for(int i = 0; i < input.length; i++){
                         for(int j = 0; j < input[0].length; j++){
@@ -82,5 +78,17 @@ public class transform{
                         }
                 }
                 return arr;
+        }
+        public static char[][] rot180(char[][] input){
+                return(rot90(rot90(input)));
+        }
+        public static char[][] rot270(char[][] input){
+                return(rot90(rot180(input)));
+        }
+
+        public static void brint(char[][] input){
+                for(char[] i: input){
+                        System.out.println(i);
+                }
         }
 }
