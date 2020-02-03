1369be86cd38ddde6756557e59490aff7ae3cf0a
Alvin <alvin.li.j@gmail.com>
2020-02-03 12:29:55 -0500

    milk

--- Greedy/milk/milk.py
+++ Greedy/milk/milk.py
@@ -16,17 +16,20 @@ for i in range(1, M+1):
     line = lines[i].split(" ")
     prices[int(line[0])] = int(line[1]) 
     ppus += [int(line[0])]
+
 ppus.sort()
-print(prices)
-print(ppus)
-totalCost = 0
-totalMilk = 0
+fin.close()
+
+totalCost, totalMilk = 0, 0
+
 for i in ppus:
-    if(totalMilk + prices.get(i) > N):
-        totalCost += (N - totalMilk) * i
+    k = max(min(prices[i], N - totalMilk), 0)
+    totalCost += k * i
+    totalMilk += k
+    if k == 0:
+        print(N - totalMilk - k)
         break
-    totalCost += i * prices.get(i)
-    totalMilk += prices.get(i)
+
 fout.write(str(totalCost) + "\n")
 fout.close()
 
