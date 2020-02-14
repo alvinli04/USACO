7c9756ddf85fa7c225c4b7c33e6387d074f0b12a
Alvin <alvin.li.j@gmail.com>
2020-02-14 13:55:46 -0500

    continued crypt1

--- Greedy/crypt1/crypt1.py
+++ Greedy/crypt1/crypt1.py
@@ -6,8 +6,21 @@ LANG: PYTHON3
 fin = open('crypt1.in', 'r')
 fout = open('crypt1.out', 'w')
 lines = fin.readlines()
-nums = [int(i) for i in lines[1].split(" ")]
-numsStr = lines[1].split(" ")
+nums = lines[1].split(" ")
 
-nums.sort()
+def checkdig(n):
+    return all(i in nums for i in str(n))
 
+works = 0
+for i in range(100, 1000):
+    if checkdig(i):
+        for j in range(10, 100):
+            if checkdig(j):
+                ps1 = i * (j//10)
+                ps2 = i * (j%10)
+                if checkdig(ps1) and checkdig(ps2) and checkdig(i*j) and i*j < 10000:
+                    works += 1
+
+fout.write(str(works) + "\n")
+fin.close()
+fout.close()
